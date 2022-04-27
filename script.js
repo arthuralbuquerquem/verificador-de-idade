e4629e343ba6b233f6f9701c11b53ea729a9e40a
Arthur Albuquerque <arthur007zinho@hotmail.com>
2022-04-27 19:02:23 -0300

    bodycolor add

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 function verificar() {
     var dadoAno = new Date().getFullYear();
     var text = document.querySelector('#resTxt');
-    var textsexo = document.getElementsByName('sexo')
+    var textsexo = document.getElementsByName('sexo');
     var dadoNasc = document.querySelector('#nasc');
-    var imagem = document.querySelector('#resImg')
+    var imagem = document.querySelector('#resImg');
+    var bgbody = document.querySelector('body');
     
     var ano = Number(dadoAno);
     var nasc = Number(dadoNasc.value);
     var idade = ano - nasc;
     
     if(dadoNasc.value == 0 || Number(dadoNasc.value) > ano ) {
-        window.alert(`[ERRO] Verifique os dados e tente novamente`)
+        window.alert(`[ERRO] Verifique os dados e tente novamente`);
     } else {
         if(textsexo[0].checked) {
             genero = 'Homem'
+            bgbody.style.background = '#0e8aff';
             if(idade >= 0 && idade < 10) {
-                imagem.src = 'images/homem-bebe.jpg'
+                imagem.src = 'images/homem-bebe.jpg';
             } else if(idade <= 19) {
-                imagem.src = 'images/homem-jovem.jpg'
+                imagem.src = 'images/homem-jovem.jpg';
             } else if(idade <= 50) {
-                imagem.src = 'images/homem-adulto.jpg'
+                imagem.src = 'images/homem-adulto.jpg';
             } else {
-                imagem.src = 'images/homem-idoso.jpg'
+                imagem.src = 'images/homem-idoso.jpg';
             }
         } else if (textsexo[1].checked) {
             genero = 'Mulher'
+            bgbody.style.background = '#f9a0fc';
             if(idade >= 0 && idade < 10) {
-                imagem.src = 'images/mulher-bebe.jpg'
+                imagem.src = 'images/mulher-bebe.jpg';
             } else if(idade <= 19) {
-                imagem.src = 'images/mulher-jovem.jpg'
+                imagem.src = 'images/mulher-jovem.jpg';
             } else if(idade <= 50) {
-                imagem.src = 'images/mulher-adulto.jpg'
+                imagem.src = 'images/mulher-adulto.jpg';
             } else {
-                imagem.src = 'images/mulher-idoso.jpg'
+                imagem.src = 'images/mulher-idoso.jpg';
             }
         }
-        text.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        text.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
     }
 }
